703d51d01d4bce665969af3441d8fa937e9eaa08
carlson172 <dirk.allmendinger@web.de>
2023-10-24 23:19:00 +0200

    comment out git pull, add --break-system-packages to pip command

--- streamLink/scripts/update.sh
+++ streamLink/scripts/update.sh
@@ -3,9 +3,9 @@
 cd /home/nak-watchdog/NAK_streamLink
 
 echo -e "=> Updating streamLink via GitHub ... \n"
-git reset --hard
-git fetch --all
-git pull --no-rebase
+#git reset --hard
+#git fetch --all
+#git pull --no-rebase
 
 echo -e '=> Installing Updates ...\n'
 sudo apt update
@@ -14,15 +14,15 @@ sudo apt upgrade -y
 # Python Package
 echo -e '=> Updating Python Packages ...\n'
 #pip install youtube_dl --upgrade
-pip install yt_dlp --upgrade
-pip install python-vlc --upgrade
-pip install tk --upgrade
-pip install RPi.GPIO --upgrade
-pip install ConfigParser --upgrade
+pip install --upgrade --break-system-packages yt_dlp
+pip install --upgrade --break-system-packages python-vlc
+pip install --upgrade --break-system-packages tk
+pip install --upgrade --break-system-packages RPi.GPIO
+pip install --upgrade --break-system-packages ConfigParse
 
 # Autostart
 echo -e '=> Adding script to autostart ...\n'
 cp ./streamLink/start_streamLink.desktop ~/.config/autostart/start_streamLink.desktop
 
 # Run config script
-echo -e "=> You can now run the config script again to select a stream location via \n ./NAK_streamLink/streamLink/scripts/config.sh"
+#echo -e "=> You can now run the config script again to select a stream location via \n ./NAK_streamLink/streamLink/scripts/config.sh"
